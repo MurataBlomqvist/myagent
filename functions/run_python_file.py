fe71951025ca249fa85852be0cd364b6d000fa12
MurataBlomqvist <adrian.blomqvist1@gmail.com>
2025-07-28 15:52:13 +0900

    fixed the file extension logic and passed the tests

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import config
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     # get the working directory
@@ -10,20 +11,24 @@ def run_python_file(working_directory, file_path, args=[]):
     tgt_path_dir = os.path.dirname(tgt_path_file)
 
     if (not tgt_path_file.startswith(wrk_dir)):
-        return f'Error: Cannot execute "{tgt_path_file}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     
     if (not os.path.exists(tgt_path_file)):
-        return f'Error: File "{tgt_path_file}" not found.'
+        return f'Error: File "{file_path}" not found.'
     
-    if (tgt_path_file[len(tgt_path_file)-2:]):
+    if (tgt_path_file[len(tgt_path_file)-3:] != ".py"):
         return f'Error: "{file_path}" is not a Python file.'
     
     try:
-        completed_prc = subprocess.run(args=args, timeout=30, capture_output=True, cwd=wrk_dir)
+        wrk_args = ["python", tgt_path_file]
+        for arg in args:
+            wrk_args.append(arg)
+        
+        completed_prc = subprocess.run(wrk_args, timeout=30, capture_output=True, cwd=wrk_dir)
         formatted_out = ""
 
         if (completed_prc.stdout != None):
-            formatted_out += f"STDOUT:{complete_prc.stdout}\n"
+            formatted_out += f"STDOUT:{completed_prc.stdout}\n"
         
         if (completed_prc.stderr != None):
             formatted_out += f"STDERR:{completed_prc.stderr}\n"
