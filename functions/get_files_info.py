0827481bd15a3ff8c232038a70a7e82a508d72c3
MurataBlomqvist <adrian.blomqvist1@gmail.com>
2025-07-30 13:12:33 +0900

    added functionality for the LLM
    added functionality for the LLM to choose which directory to list files from

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def get_files_info(working_directory, directory="."):
@@ -27,7 +28,17 @@ def get_files_info(working_directory, directory="."):
         return F"Error listing files {e}"
     
     
-#def disp_content(err_flg, directory, print_tuple=("")):
-#    print(f"Result for '{directory}' directory")
-#    if (directory):
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def exception_handler(exception_text):
-    # reducing boilerplate code in 2025 is the new vibe
+    # reducing boilerplate code in 2025 is the new vibe(coding)
     raise Exception(exception_text)
 
 def load_env_variables():
@@ -20,14 +27,31 @@ def load_env_variables():
     
     return model_name, api_key
 
-def generate_response(model_name, api_key, verbose_flag, messages):
+def generate_response(model_name, api_key, verbose_flag, system_prompt, messages):
     client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
         model=model_name
         , contents=messages
+        , config=types.GenerateContentConfig(
+            tools=[available_functions]
+            , system_instruction=system_prompt
+            )
     )
 
-    print(response.text)
+    
+    if not response.function_calls is None:
+        for functionSpec in response.function_calls:
+            #print(functionSpec)
+            func_args = ""
+            if (not functionSpec.args is None):
+                func_args = functionSpec.args
+
+            print(f"Calling function: {functionSpec.name}({func_args})")
+    else:
+        print(response.text)
+        
+    
     if verbose_flag:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
@@ -38,6 +62,18 @@ def main():
     verbose_flag = False
     # init the env variables
     model_name, api_key = load_env_variables()
+
+    system_prompt = ""
+    #init system prompt
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     print("Starting myagent!")
     if len(sys.argv) < 2:
@@ -54,7 +90,7 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_response(model_name, api_key, verbose_flag, messages)
+    generate_response(model_name, api_key, verbose_flag, system_prompt, messages)
 
 if __name__ == "__main__":
     main()
